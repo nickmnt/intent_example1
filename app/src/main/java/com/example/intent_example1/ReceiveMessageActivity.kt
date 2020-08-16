db6c65d458d0a39b55075222f5d262948e984e9d
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 12:53:45 +0430

    Second activity code

--- app/src/main/java/com/example/intent_example1/ReceiveMessageActivity.kt
+++ app/src/main/java/com/example/intent_example1/ReceiveMessageActivity.kt
@@ -2,10 +2,15 @@ package com.example.intent_example1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ReceiveMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receive_message)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = intent.getStringExtra("message")
     }
 }
