fe881c44ce89b0e4850d39c7d6b0649b83837e5b
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 12:41:33 +0430

    Finished onSendMessage

--- app/src/main/java/com/example/intent_example1/MainActivity.kt
+++ app/src/main/java/com/example/intent_example1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intent_example1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSendMessage(view: View) {
+        //In java: Intent intent = new Intent(this, ReceiveMessageActivity.class);
 
+        val intent = Intent(this, ReceiveMessageActivity::class.java)
+        startActivity(intent)
     }
 }
