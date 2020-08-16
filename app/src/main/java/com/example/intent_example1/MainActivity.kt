0f8a0ec051d3fb4b1496f28fa7ae518e43a4f48c
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 16:17:04 +0430

    Using a chooser

--- app/src/main/java/com/example/intent_example1/MainActivity.kt
+++ app/src/main/java/com/example/intent_example1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.intent_example1
 
 import android.content.Intent
+import android.content.Intent.createChooser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,9 +16,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSendMessage(view: View) {
+        //The user cannot say always use this activity
         val intent = Intent(Intent.ACTION_SEND)
         intent.setType("text/plain")
         intent.putExtra(Intent.EXTRA_TEXT, msgBox.text.toString())
-        startActivity(intent)
+        val title = getString(R.string.chooser)
+        val chosenIntent = createChooser(intent, title)
+        startActivity(chosenIntent)
     }
 }
