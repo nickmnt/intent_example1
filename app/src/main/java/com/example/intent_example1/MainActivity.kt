8251ee302494dc1a02c1bdf40884c59f21e5bbfa
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 12:47:59 +0430

    Design of second activity finished, putExtra added to send method

--- app/src/main/java/com/example/intent_example1/MainActivity.kt
+++ app/src/main/java/com/example/intent_example1/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         //In java: Intent intent = new Intent(this, ReceiveMessageActivity.class);
 
         val intent = Intent(this, ReceiveMessageActivity::class.java)
+        val msgBox = findViewById<EditText>(R.id.msgBox)
+        intent.putExtra("message", msgBox.text)
         startActivity(intent)
     }
 }
